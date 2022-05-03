19a0b887b75b0737f37cb85f9073d00a4e09bd1d
Jon Sousa <jonathonsousa@gmail.com>
2022-05-03 17:05:35 -0500

    Can now stop and play

--- main.py
+++ main.py
@@ -10,58 +10,50 @@ GUILD = guild
 intents = discord.Intents.all()
 intents.members = True
 
+queue = []
+
 client = commands.Bot(command_prefix="?", intents=intents)
 
-#https://www.youtube.com/watch?v=ml-5tXRmmFk
-#https://www.youtube.com/watch?v=jHZlvRr9KxM&t=296s
-#http://unicode.org/emoji/charts/full-emoji-list.html
+# https://www.youtube.com/watch?v=ml-5tXRmmFk
+# https://www.youtube.com/watch?v=jHZlvRr9KxM&t=296s
+# http://unicode.org/emoji/charts/full-emoji-list.html
+# https://python.land/build-discord-bot-in-python-that-plays-music
 
 
 @client.command(name="play", help="play from a random radio. play <url> play the specific song/video from YouTube")
 async def play(ctx, url: str = ""):
-    # song_there = os.path.isfile("song.mp3")
-    # try:
-    #     if song_there:
-    #         os.remove("song.mp3")
-    # except PermissionError:
-    #     await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-    #     return
     default_channels = [
-        "https://www.youtube.com/watch?v=DWcJFNfaw9c",
+        # "https://www.youtube.com/watch?v=4p2TyTyMKws",
         "https://www.youtube.com/watch?v=5qap5aO4i9A",
-        "https://www.youtube.com/watch?v=tCs48OFv7xA"
+        "https://www.youtube.com/watch?v=tCs48OFv7xA",
+        # "https://www.youtube.com/watch?v=-5KAN9_CzSA",
+        # "https://www.youtube.com/watch?v=kgx4WGK0oNU",
+        # "https://www.youtube.com/watch?v=7NOSDKb0HlU",
+        # "https://www.youtube.com/watch?v=Db-uvuvEEV0"
     ]
     if not url:
         url = random.choice(default_channels)
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voiceChannel.connect()
-    # voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    botVoiceClient = ctx.guild.voice_client
+    if botVoiceClient is None:
+        voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+        await voiceChannel.connect()
 
     await ctx.send("▶ Playing...")
+    print("▶ Playing...")
 
     ffmpeg_options = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
         'options': '-vn'
     }
     ydl_opts = {'format': 'bestaudio/best'}
-    # ydl_opts = {
-    #     'format': 'bestaudio/best',
-    #     'postprocessors': [{
-    #         'key': 'FFmpegExtractAudio',
-    #         'preferredcodec': 'mp3',
-    #         'preferredquality': '192',
-    #     }],
-    # }
-    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #     ydl.download([url])
-    # for file in os.listdir("./"):
-    #     if file.endswith(".mp3"):
-    #         os.rename(file, "song.mp3")
-    # voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+    queue.append(url)
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
+        print(info['title'])
+        await client.change_presence(activity=discord.Game(name=info['title']))
         url2 = info['formats'][0]['url']
         source = await discord.FFmpegOpusAudio.from_probe(url2, **ffmpeg_options)
         ctx.voice_client.play(source)
@@ -70,8 +62,10 @@ async def play(ctx, url: str = ""):
 @client.command(name="leave", help="I will leave the voice chat.")
 async def leave(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    await client.change_presence(activity=discord.Game(name="Just vibing..."))
     if voice.is_connected():
         goodbye_message = ["Later 🤘", "Peace out ✌", "Bye! 👋"]
+        print(random.choice(goodbye_message))
         await ctx.send(random.choice(goodbye_message))
         await voice.disconnect()
     else:
@@ -83,6 +77,7 @@ async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         await ctx.send("⏸ Paused")
+        print("⏸ Paused")
         voice.pause()
     else:
         await ctx.send("Currently no audio is playing.")
@@ -94,6 +89,7 @@ async def resume(ctx):
     if voice.is_paused():
         voice.resume()
         await ctx.send("⏯ Resumed")
+        print("⏯ Resumed")
     else:
         await ctx.send("The audio is not paused.")
 
@@ -103,20 +99,8 @@ async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
     await ctx.send("⏹ I have stopped the song, you'll need to tell me what song you'll want to play next.")
-
-
-# @client.command()
-# async def instruction(ctx):
-#     member = ctx.auther
-#     print(ctx.auther)
-#     await member.create_dm()
-#     await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!\n Here are a list of my commands:')
-#     await member.dm_channel.send('!play, play from a random radio.')
-#     await member.dm_channel.send('!play <youtube url here>, play the specific song from YouTube.')
-#     await member.dm_channel.send('!stop, this will stop the song Vibe Bot is currently playing.')
-#     await member.dm_channel.send('!pause, pause the current song.')
-#     await member.dm_channel.send('!resume, continue the paused song.')
-
+    print("⏹ I have stopped the song, you'll need to tell me what song you'll want to play next.")
+    await client.change_presence(activity=discord.Game(name="Just vibing..."))
 
 @client.event
 async def on_ready():
@@ -130,6 +114,7 @@ async def on_ready():
     )
     print(f'{client.user} is ready!')
     print("=============================")
+    await client.change_presence(activity=discord.Game(name="Just vibing..."))
 
     # for member in guild.members:
     #     # members = '\n - '.join([member.name for member in guild.members])
